774c7d739262c485d2847a18b72d49cd27c85fbc
Shamik H <shamik05@hotmail.com>
2020-08-25 19:33:56 -0400

    Added app.get for default page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,17 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-
+const db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+  // app.get("/", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/members");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/signup.html"));
+  // });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
@@ -26,4 +26,33 @@ module.exports = function(app) {
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+
+  app.get("*", (req, res) => {
+    db.Bike.findAll().then(dbBike => {
+
+      const category = [...new Set(dbBike.map(ele => ele.dataValues.category))];
+
+      const brand = [...new Set(dbBike.map(ele => ele.dataValues.brand))];
+
+      const colour = [...new Set(dbBike.map(ele => ele.dataValues.colour))];
+
+      const frameSize = [...new Set(dbBike.map(ele => ele.dataValues.framesize))];
+      
+      const frameMaterial = [...new Set(dbBike.map(ele => ele.dataValues.framematerial))];
+
+      const year = [...new Set(dbBike.map(ele => ele.dataValues.year))];
+
+      res.render("index", {
+        category: category.sort(),
+        brand: brand.sort(),
+        colour: colour.sort(),
+        frameSize: frameSize.sort(),
+        frameMaterial: frameMaterial.sort(),
+        year: year.sort(),
+        searchTotal: dbBike.length
+      });
+    });
+  });
 };
+
+//  {limit:2}
