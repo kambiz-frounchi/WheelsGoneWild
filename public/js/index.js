118e6f5f412012e251256e7fa68869c586328d69
Shamik H <shamik05@hotmail.com>
2020-08-26 23:59:43 -0400

    Added price field for renderpage function

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,50 @@
 $(document).ready(() => {
+  // Event listener for when a bike's order is clicked
+  $("[data-order]").click(() => {
+    console.log(`bikeid is ${event.target.getAttribute("data-order")}`);
+    // Code for $.Post to cart with bikeid
+    // Needs code to visually update cart items and total
+  });
+
+  $("#category li").click(() => {
+    // window.location.href += `api/bikes/filter/category/${event.target.id}`;
+    $.get("/api/bikes/filter/category/" + event.target.id, renderPage);
+  });
+
+  $("#brand li").click(() => {
+    $.get("/api/bikes/filter/brand/" + event.target.id, renderPage);
+  });
+
+  $("#frameMaterial li").click(() => {
+    $.get("/api/bikes/filter/frame_material/" + event.target.id, renderPage);
+  });
+
+  $("#year li").click(() => {
+    $.get("/api/bikes/filter/year/" + event.target.id, renderPage);
+  });
+
+  renderPage = data => {
+    console.log(data);
+    $("#bikeList").empty();
+    const items = data.map(ele => {
+      const { id, category, name, brand, year, price } = ele;
+      return `<div class="card mx-auto">
+      <img class="card-img-top" src="/images/${id}.jpg" alt="Card image cap">
+      <div class="card-body ${category}">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">${brand}  ${year}</p>
+      <p class="card-text">$ ${price}</p>
+      <div class="card-footer">
+      <button type="button" data-order="${id}" class="btn btn-secondary cardCart"><i class="fas fa-shopping-cart"></i> Add to Cart</button>
+      </div>
+      </div>
+  </div>`;
+    });
+    $("#searchTotal")[0].textContent = `${data.length} Found Results`;
+    $("#bikeList").append(items);
+  };
+
+  // Code for sync-ing slider and textbox value
   const lengthRange = document.querySelector("#lengthRange");
   const lengthNumber = document.querySelector("#lengthNumber");
 
@@ -22,48 +68,4 @@ $(document).ready(() => {
     option.value = item;
     list.appendChild(option);
   });
-
-  $("[data-order]").click(() => {
-    console.log(`bikeid is ${event.target.getAttribute("data-order")}`);
-  });
-
-  document.querySelectorAll("[data-order]");
-
-  $("#category li").click(() => {
-    // window.location.href += `api/bikes/filter/category/${event.target.id}`;
-    $.get("/api/bikes/filter/category/" + event.target.id, renderPage);
-  });
-
-  $("#brand li").click(() => {
-    $.get("/api/bikes/filter/brand/" + event.target.id, renderPage);
-  });
-
-  $("#frameMaterial li").click(() => {
-    $.get("/api/bikes/filter/frame_material/" + event.target.id, renderPage);
-  });
-
-  $("#year li").click(() => {
-    $.get("/api/bikes/filter/year/" + event.target.id, renderPage);
-  });
-
-  renderPage = data => {
-    console.log(data);
-    $("#bikeList").empty();
-    const items = data.map(ele => {
-      const { id, category, name, brand, year } = ele;
-      return `<div class="card mx-auto">
-      <img class="card-img-top" src="/images/${id}.jpg" alt="Card image cap">
-      <div class="card-body ${category}">
-          <h5 class="card-title">${name}</h5>
-          <p class="card-text">${brand}  ${year}</p>
-  
-      <div class="card-footer">
-          <button type="button" class="btn btn-secondary">Order with placeholder Price</button>
-      </div>
-      </div>
-  </div>`;
-    });
-    $("#searchTotal")[0].textContent = `${data.length} Found Results`;
-    $("#bikeList").append(items);
-  };
 });
