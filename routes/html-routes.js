7c4e1df6598041e3cee131a6ad96ffe615d746e1
Shamik H <shamik05@hotmail.com>
2020-08-28 22:57:52 -0400

    Changed href and html routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,22 +5,27 @@ const db = require("../models");
 //const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // app.get("/", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.sendFile(path.join(__dirname, "../public/signup.html"));
-  // });
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
+  // Route for logging user out
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
   /*
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
